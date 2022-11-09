49c298b655ad6ead64a50835f64911e9977fdf1f
MEGALITH2012\DFI <d.flakowski@megalith-software.de>
2022-11-09 14:50:46 +0100

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -35,6 +36,16 @@ namespace API.Controllers
             return await _userRepository.GetMemberAsync(username);
             //var user=await _userRepository.GetUserByUsernameAsync(username);
             //return _mapper.Map<MemberDto>(user);
-        }	
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username=User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user=await _userRepository.GetUserByUsernameAsync(username);
+            _mapper.Map(memberUpdateDto, user);
+            _userRepository.Update(user);
+            if (await _userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update User");
+        }
     }
 }
